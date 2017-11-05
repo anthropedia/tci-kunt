a39160ac4e3ee48a76dd1c9d2800213ec3ba42ef
Vincent Agnano <vincent.agnano@scopyleft.fr>
2017-11-05 16:36:34 -0500

    fixed tests

--- core/__init__.py
+++ core/__init__.py
@@ -1,10 +1,15 @@
 import os
+import sys
 
 from flask import Flask
 from tciminne import db
 
+settings_file = 'settings'
+if 'unittest' in sys.argv[0]:
+    settings_file = 'settings.test'
+
 PROJECT_PATH = os.path.abspath(os.path.dirname(__file__) + '/..')
-os.environ.setdefault('SETTINGS', PROJECT_PATH + '/settings.py')
+os.environ.setdefault('SETTINGS', f'{PROJECT_PATH}/{settings_file}.py')
 
 app = Flask('tci-kunt')
 app.config.from_envvar('SETTINGS')
@@ -13,3 +18,5 @@ app.root_path = PROJECT_PATH
 app.secret_key = app.config['SECRET_KEY']
 
 db.connect(**app.config['DATABASE'])
+
+from core import views, templates  # noqa: F401
--- core/views.py
+++ core/views.py
@@ -1,17 +1,50 @@
-from flask import render_template, request
+from functools import wraps
+
+from flask import render_template, request, redirect, url_for, g
 from tciminne.models import Token, Score
 from tcidata import get_tci
 
 from . import app
 
 
+def token_required(func):
+    @wraps
+    def wrapper(*args, **kwargs):
+        # token = kwargs.get('token')
+        # import ipdb; ipdb.set_trace()
+        # try:
+        #     token_obj = Token.objects.get(key=token, usage_date=None)
+        #     g.language = token_obj.language
+        # except Token.DoesNotExist:
+        #     return redirect(url_for('error'))
+        return func(*args, **kwargs)
+    return wrapper
+
+
+def is_token_valid(token):
+    try:
+        Token.objects.get(key=token, usage_date=None)
+        return True
+    except Token.DoesNotExist:
+        return False
+
+
 @app.route('/')
-def home():
+def error():
     return render_template('error.html'), 401
 
 
-@app.route('/<string:token>')
+@app.route('/<string:token>/')
+def home(token):
+    if not is_token_valid(token):
+        return redirect(url_for('error'))
+    return render_template('intro.html')
+
+
+@app.route('/<string:token>/run/')
 def survey(token):
+    if not is_token_valid(token):
+        return redirect(url_for('error'))
     try:
         token = Token.objects.get(key=token, usage_date=None)
     except Token.DoesNotExist:
@@ -21,7 +54,7 @@ def survey(token):
     return render_template('survey.html', questions=questions, token=token)
 
 
-@app.route('/end', methods=['post'])
+@app.route('/end/', methods=['post'])
 def end():
     token = Token.objects.get(key=request.form.get('token'))
     answers = [int(a) for a in request.form.get('answers').split(',')]
